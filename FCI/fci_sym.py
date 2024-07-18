15e40060e6c81c1f82b5832218928b8b81e136a2
JannisErhard <jannis.erhard@gmx.de>
2024-07-18 07:53:42 +0200

    minor improvements in readability

--- FCI/fci_sym.py
+++ FCI/fci_sym.py
@@ -17,13 +17,19 @@ import psutil
 
 process = psutil.Process()
 
+# what is tested
 Run_With_Sym, Run_Without_Sym = True, False
+
+# setup of calculations
+basis = '631g'
+element = 'B'
+r = 4.0
 
 # Assign FCI wavefunction symmetry
 
 if Run_With_Sym:
     start_time = time.time() # timing
-    mol = gto.M(atom='B 0 0 0;  B 0 0 4.0', basis='631g', symmetry=True, charge=0)
+    mol = gto.M(atom=f'{element} 0 0 0;  {element} 0 0 {r}', basis=basis, symmetry=True, charge=0)
     m = scf.RHF(mol).run()
     norb = m.mo_coeff.shape[1]
     nelec = mol.nelec
@@ -38,7 +44,7 @@ if Run_With_Sym:
 
 if Run_Without_Sym:
     start_time = time.time()
-    mol = gto.M(atom='B 0 0 0;  B 0 0 4.0', basis='631g', symmetry=False, charge=0)
+    mol = gto.M(atom='{element} 0 0 0;  {element} 0 0 {r}', basis=basis, symmetry=False, charge=0)
     m = scf.RHF(mol).run()
     norb = m.mo_coeff.shape[1]
     nelec = mol.nelec
@@ -48,3 +54,10 @@ if Run_Without_Sym:
     print("--- %s seconds ---" % (time.time() - start_time))
 
 print(f"Programs memory usage {process.memory_info().rss/ 1024 ** 2} MiB")  # in bytes 
+
+# Result True, False:
+#converged SCF energy = -48.9306158661173
+#Energy of A1g state -49.128747751762
+#--- 452.160982131958 seconds ---
+#Programs memory usage 809.0859375 MiB
+
